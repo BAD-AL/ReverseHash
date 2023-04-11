45b43170c041df77ba5a2bab6854e9dd38a03dae
Chris Badal <chris.f.badal@hp.com>
2023-04-10 22:12:15 -0600

    added gui

--- Program.cs
+++ Program.cs
@@ -2,97 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace ReverseHash
 {
+
+    public interface IAppendText
+    {
+        void AppendText(string s);
+    }
+
     public class Program
     {
-
-        static uint HashString(char[] input)
-        {
-            uint FNV_prime = 16777619;
-            uint offset_basis = 2166136261;
-            uint hash = offset_basis;
-            byte c = 0;
-            foreach (char c_ in input)
-            {
-                c = (byte)c_;
-                c |= 0x20;
-                hash ^= c;
-                hash *= FNV_prime;
-            }
-            return hash;
-        }
-
-        public static void PrintMatches(
-            UInt32 hash,
-            string possibleCharacters,
-            int targetStringSizeBegin,
-            int targetStringSizeEnd,
-            string prefix
-        )
-        {
-            DateTime start = DateTime.Now;
-            Console.WriteLine("Searching matches for 0x{0:x}\n with prefix '{1}'\n using char set '{2}'\n startLen: {3}\n endLen: {4} (range: {5})",
-                hash, prefix, possibleCharacters, targetStringSizeBegin, targetStringSizeEnd, (targetStringSizeEnd - targetStringSizeBegin));
-            for (int i = targetStringSizeBegin; i < targetStringSizeEnd; i++)
-            {
-                Console.WriteLine("searching string length = {0}; guess_len:{1}", i, i - prefix.Length);
-                PrintMatches(hash, possibleCharacters, i, prefix);
-            }
-            DateTime end = DateTime.Now;
-            var timeTaken = end - start;
-            Console.WriteLine("Finished Searching matches for 0x{0:x}; time: {1}ms ", hash, timeTaken.Milliseconds);
-        }
-
-        static void PrintMatches(uint hash, string possibleCharacters, int targetStringSize, string prefix)
-        {
-            char[] bufferToUse = new char[targetStringSize];
-            prefix.CopyTo(0, bufferToUse, 0, prefix.Length);
-            GenerateStringsAndCheckHash(prefix.Length, targetStringSize, hash, possibleCharacters, bufferToUse);
-        }
-
-        static void GenerateStringsAndCheckHash(int index, int targetStringSize, uint targetHash, string possibleCharacters, char[] bufferToUse)
-        {
-            if (index == targetStringSize)
-            {
-                if (HashString(bufferToUse) == targetHash)
-                {
-                    Console.WriteLine("Match found: " + new string(bufferToUse));
-                }
-            }
-            else
-            {
-                foreach (char c in possibleCharacters)
-                {
-                    bufferToUse[index] = c;
-                    GenerateStringsAndCheckHash(index + 1, targetStringSize, targetHash, possibleCharacters, bufferToUse);
-                }
-            }
-        }
-
+        static string possibleCharacters = "abcdefghijklmnopqrstuvwxyz.0123456789";
+        static int maxGuessLen = 8;
         static void Main(string[] args)
         {
             uint hash = 0;
-            string possibleCharacters = "abcdefghijklmnopqrstuvwxyz.0123456789";
-            int maxGuessLen = 8;
+
             string prefix = "";
 
             // Parse command line arguments
-            if (args.Length < 1)
+            if (args.Length == 0)
             {
-                Console.Error.WriteLine("Usage: ReverseHash <hash> [-c:<character_set>] [-l:<string_length>] [-p:<prefix>]");
-                Console.Error.WriteLine("Defaults:[-c:{0}] [-l:{1}]", possibleCharacters, maxGuessLen);
-                Environment.Exit(1);
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new ReverseHashForm());
             }
             else
             {
+                if (args[0].StartsWith("-h"))
+                {
+                    PrintHelp();
+                }
+                    
                 string hashStr = args[0];
                 if (hashStr.StartsWith("0x"))
                     hash = Convert.ToUInt32(hashStr.Substring(2), 16);
                 else
                     hash = Convert.ToUInt32(hashStr);
-                
+
                 for (int i = 1; i < args.Length; i++)
                 {
                     string arg = args[i];
@@ -114,11 +63,104 @@ namespace ReverseHash
                         Environment.Exit(1);
                     }
                 }
-            }
 
-            //PrintMatches(hash, possibleCharacters, targetStringSize, prefix);
-            PrintMatches( hash, possibleCharacters, 
-                prefix.Length + 1, prefix.Length + maxGuessLen, prefix );
+                //PrintMatches(hash, possibleCharacters, targetStringSize, prefix);
+                HashReverse hr = new HashReverse();
+                hr.PrintMatches(hash, possibleCharacters,
+                    prefix.Length + 1, prefix.Length + maxGuessLen, prefix);
+            }
+        }
+
+        private static void PrintHelp()
+        {
+            Console.Error.WriteLine("Usage: ReverseHash <hash> [-c:<character_set>] [-l:<string_length>] [-p:<prefix>]");
+            Console.Error.WriteLine("Defaults:[-c:{0}] [-l:{1}]", possibleCharacters, maxGuessLen);
+            Console.Error.WriteLine(" Example:\n        ReverseHash.exe 0xa7d91d67 -c:{0} -l:{1} -p:is", possibleCharacters, maxGuessLen);
+            Environment.Exit(1);
+        }
+    }
+
+    public class HashReverse
+    {
+        private IAppendText appendObj = null;
+
+        public IAppendText AppendObj
+        {
+            get { return appendObj; }
+            set { appendObj = value; }
+        }
+
+        void Print(string s)
+        {
+            if (!s.EndsWith("\n"))
+                s += "\n";
+            if (appendObj == null)
+                Console.Write(s);
+            else
+                appendObj.AppendText(s);
+        }
+
+        uint HashString(char[] input)
+        {
+            uint FNV_prime = 16777619;
+            uint offset_basis = 2166136261;
+            uint hash = offset_basis;
+            byte c = 0;
+            foreach (char c_ in input)
+            {
+                c = (byte)c_;
+                c |= 0x20;
+                hash ^= c;
+                hash *= FNV_prime;
+            }
+            return hash;
+        }
+
+        public void PrintMatches(
+            UInt32 hash,
+            string possibleCharacters,
+            int targetStringSizeBegin,
+            int targetStringSizeEnd,
+            string prefix
+        )
+        {
+            DateTime start = DateTime.Now;
+            Print(string.Format("Searching matches for 0x{0:x}\n with prefix '{1}'\n using char set '{2}'\n startLen: {3}\n endLen: {4} (range: {5})\n",
+                hash, prefix, possibleCharacters, targetStringSizeBegin, targetStringSizeEnd, (targetStringSizeEnd - targetStringSizeBegin)));
+            for (int i = targetStringSizeBegin; i < targetStringSizeEnd; i++)
+            {
+                Print(string.Format("searching string length = {0}; guess_len:{1}", i, i - prefix.Length));
+                PrintMatches(hash, possibleCharacters, i, prefix);
+            }
+            DateTime end = DateTime.Now;
+            var timeTaken = end - start;
+            Print(string.Format("Finished Searching matches for 0x{0:x}; time: {1}ms ", hash, timeTaken.Milliseconds));
+        }
+
+        void PrintMatches(uint hash, string possibleCharacters, int targetStringSize, string prefix)
+        {
+            char[] bufferToUse = new char[targetStringSize];
+            prefix.CopyTo(0, bufferToUse, 0, prefix.Length);
+            GenerateStringsAndCheckHash(prefix.Length, targetStringSize, hash, possibleCharacters, bufferToUse);
+        }
+
+        void GenerateStringsAndCheckHash(int index, int targetStringSize, uint targetHash, string possibleCharacters, char[] bufferToUse)
+        {
+            if (index == targetStringSize)
+            {
+                if (HashString(bufferToUse) == targetHash)
+                {
+                    Print(string.Format("Match found: '{0}'", new string(bufferToUse)));
+                }
+            }
+            else
+            {
+                foreach (char c in possibleCharacters)
+                {
+                    bufferToUse[index] = c;
+                    GenerateStringsAndCheckHash(index + 1, targetStringSize, targetHash, possibleCharacters, bufferToUse);
+                }
+            }
         }
     }
 }
---
+++ ReverseHashForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ReverseHash
+{
+    public partial class ReverseHashForm : Form, IAppendText
+    {
+        public ReverseHashForm()
+        {
+            InitializeComponent();
+        }
+
+        #region IAppendText Members
+
+        public void AppendText(string s)
+        {
+            if (IsHandleCreated && InvokeRequired)
+            {
+                // Invoke an anonymous method on the thread of the form.
+                this.Invoke((MethodInvoker)delegate
+                {
+                    AppendText(s);
+                });
+            }
+            else
+            {
+                txtOutput.AppendText(s);
+            }
+        }
+
+        private void SetGoButtonText(string s)
+        {
+            if (IsHandleCreated && InvokeRequired)
+            {
+                // Invoke an anonymous method on the thread of the form.
+                this.Invoke((MethodInvoker)delegate {
+                    SetGoButtonText(s);
+                });
+            }
+            else
+            {
+                btnGo.Text = s;
+            }
+        }
+
+        #endregion
+
+        Thread workerThread = null;
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (workerThread == null)
+            {
+                txtOutput.Clear();
+                _prefix = txtPrefix.Text;
+                string hashStr = txtHash.Text;
+
+                if (hashStr.StartsWith("0x"))
+                    _hashToLookUp = Convert.ToUInt32(hashStr.Substring(2), 16);
+                else
+                    _hashToLookUp = Convert.ToUInt32(hashStr);
+                _maxLen = (int)spinLen.Value;
+                _possibleChars = txtPossibleCharacters.Text;
+
+                btnGo.Text = "Stop";
+                workerThread = new Thread(new ThreadStart(DoWork));
+                workerThread.Start();
+            }
+            else
+            {
+                workerThread.Abort();
+                workerThread = null;
+                SetGoButtonText("&Go");
+                AppendText("Worker Thread killed");
+            }
+        }
+
+        private string _prefix = "";
+        private string _possibleChars = "";
+        private UInt32 _hashToLookUp = 0;
+        int _maxLen = 0;
+
+        private void DoWork()
+        {
+            HashReverse hr = new HashReverse();
+            hr.AppendObj = this;
+            AppendText( string.Format("Running command:\n  ReverseHash 0x{0} -p:{1} -c:{2} -l:{3}\n", 
+                _hashToLookUp, _prefix, _possibleChars, _maxLen));
+            hr.PrintMatches(_hashToLookUp, _possibleChars,
+               _prefix.Length + 1, _prefix.Length + _maxLen, _prefix);
+            SetGoButtonText("&Go");
+        }
+
+        private void bthShowCmdLine_Click(object sender, EventArgs e)
+        {
+            _prefix = txtPrefix.Text;
+            string hashStr = txtHash.Text;
+
+            if (hashStr.StartsWith("0x"))
+                _hashToLookUp = Convert.ToUInt32(hashStr.Substring(2), 16);
+            else
+                _hashToLookUp = Convert.ToUInt32(hashStr);
+            _maxLen = (int)spinLen.Value;
+            _possibleChars = txtPossibleCharacters.Text;
+
+            AppendText(string.Format("  ReverseHash.exe 0x{0} -p:{1} -c:{2} -l:{3}\n",
+                    _hashToLookUp, _prefix, _possibleChars, _maxLen));
+        }
+    }
+}
